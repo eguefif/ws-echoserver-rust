4c83d4493c2bb41c17f1a0ae02ff3bba9d2ac388
Emmanuel Guefif <eguefif@fastmail.com>
2025-03-30 20:13:27 -0400

    Error handling in client

---
+++ websocket-rs/src/websocket_client.rs
@@ -0,0 +1,91 @@
+use crate::websocket_error::WsError;
+use crate::websocket_server::process_key;
+use base64::prelude::*;
+use rand::Rng;
+use std::error;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+pub struct WebSocketClient {
+    socket: TcpStream,
+}
+
+impl WebSocketClient {
+    pub fn new(ip: &str, port: i32) -> Result<Self, Box<dyn error::Error>> {
+        let mut socket = TcpStream::connect(format!("{}:{}", ip, port))?;
+        let key = generate_key();
+        let request = build_request(&key);
+        socket.write_all(request.as_bytes())?;
+        check_server_response(&mut socket, key)?;
+        Ok(Self { socket })
+    }
+
+    pub fn send_frame(&mut self, payload: &str) {
+        println!("Sending: {payload}");
+    }
+
+    pub fn read_frame(&mut self) -> String {
+        "Hello World".to_string()
+    }
+}
+
+fn generate_key() -> String {
+    let mut key = [0u8; 16];
+    rand::rng().fill(&mut key);
+    BASE64_STANDARD.encode(key)
+}
+
+fn build_request(key: &str) -> String {
+    format!(
+        " GET / HTTP/1.1\r\n\
+Sec-WebSocket-Version: 13\r\n\
+Sec-WebSocket-Key: {key}\r\n\
+Connection: Upgrade\r\n\
+Upgrade: websocket\r\n\
+Host: 127.0.0.1:8000\r\n\r\n
+    "
+    )
+}
+
+fn check_server_response(socket: &mut TcpStream, key: String) -> Result<(), Box<dyn error::Error>> {
+    let response = read_server_http_response(socket)?;
+    let client_key = extract_client_key(&response)?;
+    let control_key = get_control_key(&key);
+    if client_key == control_key {
+        Ok(())
+    } else {
+        Err(Box::new(WsError::InvalidHandshakeKey))
+    }
+}
+
+fn read_server_http_response(socket: &mut TcpStream) -> std::io::Result<String> {
+    let mut buffer = vec![0; 1024];
+    let mut response = String::new();
+    loop {
+        socket.read(&mut buffer)?;
+        let chunk = String::from_utf8_lossy(&buffer);
+        if chunk.contains("\r\n\r\n") {
+            let chunk = chunk.split("\r\n\r\n").next().unwrap();
+            response.push_str(&chunk);
+            return Ok(response);
+        }
+        response.push_str(&chunk);
+    }
+}
+
+fn extract_client_key(response: &str) -> Result<String, Box<dyn error::Error>> {
+    for line in response.lines() {
+        if line.contains("Sec-WebSocket-Accept") {
+            if let Some((_, value)) = line.split_once(":") {
+                return Ok(value.trim().to_string());
+            } else {
+                return Err(Box::new(WsError::Header(line.to_string())));
+            }
+        }
+    }
+    Err(Box::new(WsError::MissingSecWebSocketAcceptHeader))
+}
+
+fn get_control_key(key: &str) -> String {
+    process_key(key)
+}
---
+++ websocket-rs/src/websocket_server.rs
@@ -0,0 +1,95 @@
+use base64::prelude::*;
+use sha1::{Digest, Sha1};
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+pub struct WebSocketServer {
+    socket: TcpStream,
+}
+
+impl WebSocketServer {
+    pub fn new(mut socket: TcpStream) -> std::io::Result<Self> {
+        let client_request = read_http_request(&mut socket);
+        let key = extract_client_key(client_request);
+        let response = build_response(key);
+        socket.write_all(response.as_bytes())?;
+        Ok(Self { socket })
+    }
+
+    pub fn try_read_frame(&mut self) -> Option<String> {
+        Some("Hey".to_string())
+    }
+
+    pub fn send_frame(&mut self, payload: String) {
+        println!("Sending: {payload}");
+    }
+}
+
+fn read_http_request(socket: &mut TcpStream) -> String {
+    let mut retval = String::new();
+    let mut buffer = vec![0; 1024];
+    loop {
+        if let Ok(_) = socket.read(&mut buffer) {
+            let chunk = String::from_utf8_lossy(&buffer);
+            if chunk.contains("\r\n\r\n") {
+                let mut splits = chunk.split("\r\n\r\n");
+                let last_chunk = splits.next().unwrap();
+                retval.push_str(last_chunk);
+                break;
+            } else {
+                retval.push_str(&chunk);
+            }
+        }
+    }
+    retval
+}
+
+fn extract_client_key(client_request: String) -> String {
+    for line in client_request.lines() {
+        if line.contains("Sec-WebSocket-Key") {
+            let mut splits = line.split(":");
+            splits.next().expect("Error: header wrong format");
+            return splits
+                .next()
+                .expect("Error: no value for websocket key")
+                .trim()
+                .to_string();
+        }
+    }
+    panic!("Error: not a valid websocket upgrade request")
+}
+
+fn build_response(key: String) -> String {
+    let server_key = process_key(&key);
+    format!(
+        "HTTP/1.1 101 Switching Protocols\r\n\
+Upgrade: websocket\r\n\
+Connection: Upgrade\r\n\
+Sec-WebSocket-Accept: {}\r\n\r\n
+",
+        server_key
+    )
+}
+
+pub fn process_key(key: &str) -> String {
+    let guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    let concatenated_key = format!("{}{}", key, guid);
+    let mut hasher = Sha1::new();
+    hasher.update(concatenated_key);
+    let digest = hasher.finalize();
+    BASE64_STANDARD.encode(digest)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_should_processed_key() {
+        let client_key = "dGhlIHNhbXBsZSBub25jZQ==";
+        let expected = "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=";
+
+        let result = process_key(client_key);
+        assert_eq!(result, expected);
+    }
+}
